178318e2b85084501b35c98ad0a527dd847b29ba
Николай <snkku@yandex.ru>
2023-05-08 18:54:17 +0500

    Наконец-то...

---
+++ src/main/kotlin/ru/netology/Main.kt
@@ -0,0 +1,8 @@
+package ru.netology
+
+import Comment
+import Note
+import NotesService
+
+fun main() {
+}
---
+++ src/test/kotlin/ru/netology/NotesServiceTest.kt
@@ -0,0 +1,59 @@
+package ru.netology
+
+
+import Comment
+import Note
+import NotesService
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+class NotesServiceTest {
+    val ns = NotesService
+
+    @Before
+    fun initiate() {
+        ns.add(Note(0, "Test", "Test1", mutableListOf()))
+        ns.add(Note(0, "Test2", "Test2", mutableListOf()))
+        ns.add(Note(0, "Test3", "Test3", mutableListOf()))
+        ns.add(Note(0, "Test4", "Test4", mutableListOf()))
+        ns.addComment(2, Comment(1, "sadsadsadsad"))
+    }
+
+    @Test
+    fun addNote() {
+        val id = ns.add(Note(10, "Test10", "Test text", mutableListOf()))
+        assertNotEquals(id, 0)
+    }
+
+    @Test
+    fun addComment() {
+        ns.addComment(3, Comment(0, "Test comment"))
+        assertEquals("Test comment", ns.getComments(3)!![0].message)
+    }
+
+    @Test
+    fun getComments() {
+        assertNull(ns.getComments(3))
+    }
+
+    @Test
+    fun edit() {
+        val noteReplace = Note(10, "Replaced", "Aasdsdd", mutableListOf())
+        ns.edit(3, noteReplace)
+        assertEquals("Replaced", ns.get(10, ns.list)!!.title)
+    }
+
+    @Test
+    fun getById() {
+        val id = ns.get(3, ns.list)!!.id
+        assertEquals(id, 3)
+    }
+
+    @Test
+    fun remove() {
+        ns.remove(10)
+        assertNull(ns.get(10, ns.list))
+    }
+}
